1799d3b86ecf5277f3a61eb36e0442942e1f84fa
mazeta1910 <matheuscps110@gmail.com>
2025-09-17 14:58:39 -0300

    Add new SQL exercises and update existing solutions
    Added a new SQL script 'Resolução Lista Manus.sql' with schema and data for an online store, and included a related PDF schema. Updated 'Resolução Lista Exercícios 03.sql' with a new database structure and queries, replacing the previous exercise content. Expanded 'Conteudo quadro.txt' with explanations of SQL JOIN types.

--- Setembro/15-09-25/Resolução Lista Exercícios 03.sql
+++ Setembro/15-09-25/Resolução Lista Exercícios 03.sql
@@ -1,161 +1,126 @@
 USE master;
 GO
-DROP DATABASE ListaDeExercicios02;
+DROP DATABASE ListaDeExercicios03;
 
-CREATE DATABASE ListaDeExercicios02;
-
-CREATE TABLE Cliente(
-CodCliente int CONSTRAINT PK_CODCLIENTE PRIMARY KEY IDENTITY(1,1),
-Nome varchar(200) CONSTRAINT NN_NOME_CLIENTE NOT NULL,
-Cidade varchar(200) CONSTRAINT NN_NOME_CIDADE NOT NULL
-);
+CREATE DATABASE ListaDeExercicios03;
+USE ListaDeExercicios03;
 
 CREATE TABLE Produto(
-CodProduto int CONSTRAINT PK_CODPRODUTO PRIMARY KEY IDENTITY(1,1),
-NomeProduto varchar(200) CONSTRAINT NN_NOME_PRODUTO NOT NULL,
+CodProduto int CONSTRAINT PK_COD_PRODUTO PRIMARY KEY IDENTITY(1,1),
+Nome varchar(10) CONSTRAINT NN_NOME_PRODUTO NOT NULL,
 Preco decimal(8,2) CONSTRAINT NN_PRECO_PRODUTO NOT NULL,
-Tipo varchar(10) CONSTRAINT CK_TIPO_PRODUTO CHECK (Tipo IN ('Hardware', 'Software', 'Outros'))
+Tipo varchar(10) CONSTRAINT NN_TIPO_PRODUTO NOT NULL
+);
+
+CREATE TABLE StatusConta(
+CodStatusConta int CONSTRAINT PK_COD_STATUS_Conta PRIMARY KEY IDENTITY (1,1),
+Nome varchar(50) CONSTRAINT CK_NOME_STATUS_CONTA CHECK (Nome IN ('Aberto', 'Fechado', 'Pago'))
+);
+
+CREATE TABLE StatusPedido(
+CodStatusPedido int CONSTRAINT PK_COD_STATUS_PEDIDO PRIMARY KEY IDENTITY (1,1),
+Nome varchar(50) CONSTRAINT CK_NOME_STATUS_PEDIDO CHECK (Nome IN ('Pendente', 'Entregue')),
+);
+
+CREATE TABLE StatusUsuario(
+CodStatusUsuario int CONSTRAINT PK_COD_STATUS_USUARIO PRIMARY KEY IDENTITY (1,1),
+Nome varchar(50) CONSTRAINT CK_NOME_STATUS_USUARIO CHECK (Nome IN ('Ativo', 'Inativo'))
+);
+
+CREATE TABLE Conta(
+CodConta int CONSTRAINT PK_COD_CONTA PRIMARY KEY IDENTITY (1,1),
+Mesa int CONSTRAINT NN_MESA NOT NULL,
+HoraAbertura datetime CONSTRAINT NN_HORAABERTURA NOT NULL,
+HoraFechamento datetime CONSTRAINT NN_HORAFECHAMENTO NOT NULL,
+Situacao int CONSTRAINT NN_SITUACAO_CONTA NOT NULL,
+CONSTRAINT FK_SITUACAO_CONTA FOREIGN KEY (Situacao)
+REFERENCES StatusConta(CodStatusConta),
+Total decimal(8,2) CONSTRAINT NN_TOTAL_CONTA NOT NULL
+);
+
+CREATE TABLE Usuario(
+CodUsuario int CONSTRAINT PK_COD_USUARIO PRIMARY KEY IDENTITY (1,1),
+Login varchar(100) CONSTRAINT NN_LOGIN_USUARIO NOT NULL,
+CONSTRAINT UQ_LOGIN_UNIQUE UNIQUE(Login),
+Senha varchar(50) CONSTRAINT NN_SENHA_USUARIO NOT NULL,
+UltimoAcesso datetime CONSTRAINT NN_ULTIMO_ACESSO NOT NULL,
+Status int CONSTRAINT NN_STATUS_USUARIO NOT NULL,
+CONSTRAINT FK_STATUS_USUARIO FOREIGN KEY (Status)
+REFERENCES StatusUsuario(CodStatusUsuario)
 );
 
 CREATE TABLE Pedido(
-CodPedido int CONSTRAINT PK_CODPEDIDO PRIMARY KEY IDENTITY(1,1),
+CodPedido int CONSTRAINT PK_COD_PEDIDO PRIMARY KEY IDENTITY (1,1),
 Produto int CONSTRAINT NN_PRODUTO_PEDIDO NOT NULL,
-Cliente int CONSTRAINT NN_CLIENTE_PEDIDO NOT NULL,
-ValorUnitario decimal(8,2) CONSTRAINT NN_VALORUNIT_PEDIDO NOT NULL,
-ValorTotal decimal(8,2) CONSTRAINT NN_VALORTOTAL_PEDIDO NOT NULL,
-CONSTRAINT FK_PRODUTO_PEDIDO FOREIGN KEY (Produto) REFERENCES Produto(CodProduto),
-CONSTRAINT FK_CLIENTE_PEDIDO FOREIGN KEY (Cliente) REFERENCES Cliente(CodCliente)
+Qtde int CONSTRAINT NN_QTD NOT NULL,
+ValorUnitario decimal(8,2) CONSTRAINT NN_VALOR_UNITARIO NOT NULL,
+ValorTotal decimal(8,2) CONSTRAINT NN_VALOR_TOTAL NOT NULL,
+Situacao int CONSTRAINT NN_SITUACAO_PEDIDO NOT NULL,
+Conta int CONSTRAINT NN_CONTA_PEDIDO NOT NULL,
+Usuario int CONSTRAINT NN_USUARIO_PEDIDO NOT NULL,
+CONSTRAINT FK_PRODUTO_PEDIDO FOREIGN KEY (Produto)
+REFERENCES Produto(CodProduto),
+CONSTRAINT FK_SITUACAO_PEDIDO FOREIGN KEY (Situacao)
+REFERENCES StatusPedido(CodStatusPedido),
+CONSTRAINT FK_CONTA_PEDIDO FOREIGN KEY (Conta)
+REFERENCES Conta(CodConta),
+CONSTRAINT FK_USUARIO_PEDIDO FOREIGN KEY (Usuario)
+REFERENCES Usuario(CodUsuario)
 );
 
---a) Insira 3 produtos, 3 clientes e 4 pedidos no banco de dados.
-INSERT INTO Produto(NomeProduto, Preco, Tipo)
-VALUES ('AutoCAD', 7500, 'Software');
-INSERT INTO Produto(NomeProduto, Preco, Tipo)
-VALUES ('NVIDIA GeForce GTX6060', 4500, 'Hardware');
-INSERT INTO Produto(NomeProduto, Preco, Tipo)
-VALUES ('Mouse Razer DeathAdder V3 Pro', 980, 'Outros');
-INSERT INTO Produto(NomeProduto, Preco, Tipo)
-VALUES ('Retrado da Santina', 16.50, 'Outros');
+-- Crie os comandos SQL para inserir 2 registros na tabela status pedido.
+INSERT INTO StatusPedido(Nome)
+VALUES('Pendente');
+INSERT INTO StatusPedido(Nome)
+VALUES('Entregue');
 
-INSERT INTO Cliente(Nome, Cidade)
-VALUES ('Lucas Renan Ribeiro Straube', 'Itaperuçu');
-INSERT INTO Cliente(Nome, Cidade)
-VALUES ('Franscico Dindão', 'Coivaras');
-INSERT INTO Cliente(Nome, Cidade)
-VALUES ('Matheus Honorato', 'Duque de Caxias');
+-- Crie outro comando SQL para inserir 2 status de usuário (ativo e inativo)
+INSERT INTO StatusUsuario(Nome)
+VALUES('Inativo');
+INSERT INTO StatusUsuario(Nome)
+VALUES('Ativo')
 
-INSERT INTO Pedido(Produto, Cliente, ValorUnitario, ValorTotal)
-VALUES (1, 1, 7500, 7500);
-INSERT INTO Pedido(Produto, Cliente, ValorUnitario, ValorTotal)
-VALUES (2, 2, 4500, 4500);
-INSERT INTO Pedido(Produto, Cliente, ValorUnitario, ValorTotal)
-VALUES (3, 3, 980, 980);
-INSERT INTO Pedido(Produto, Cliente, ValorUnitario, ValorTotal)
-VALUES (1, 3, 7500, 7500);
+-- Crie outro comando SQL para inserir 3 status da conta (Aberto, Fechado, Pago)
+INSERT INTO StatusConta(Nome)
+VALUES('Fechado');
+INSERT INTO StatusConta(Nome)
+VALUES('Pago');
+INSERT INTO StatusConta(Nome)
+VALUES('Aberto');
 
--- 2) Exclua todos os produtos que o preço esteja entre 1 real e 2,99.
-DELETE FROM Produto
-WHERE Preco BETWEEN 1.00 AND 2.99;
+-- Faça um comando para excluir todos os registros da tabela peDido, porém, que o usuário que fez o pedido tenha o nome Antonio (exatamente como Antonio)
+DELETE FROM PEDIDO
+WHERE Usuario IN (SELECT CodUsuario FROM Usuario WHERE Login = 'Antonio');
 
--- 3) Exclua os clientes que não informaram em qual cidade moram.
-DELETE FROM Cliente
-Where Cidade IS NULL;
-
--- 4) Atualize o preço de todos os produtos do tipo Software. Dê um aumento de 10%.
+-- Aumente em 10% o preço de cada produto da tabela produto.
 UPDATE Produto
-SET Preco = Preco * 1.1
-Where Tipo LIKE 'Software';
+Set Preco = Preco*1.10;
 
--- 5) Liste o nome de todos os produtos que o tipo é Hardware ou Software, e ordene por nome do produto.
-SELECT CodProduto, NomeProduto, Preco
+-- Liste o nome de cada garçom (usuário) e o valor total em pedidos que cada um fez.
+SELECT U.Login, SUM(P.ValorTotal) AS 'Valor Total em Pedidos'
+FROM Usuario U
+INNER JOIN Pedido P ON P.Usuario = U.CodUsuario
+GROUP BY U.Login;
+
+-- Mostre a soma do valor total de todas as contas que não tiveram data e hora de fechamento.
+SELECT SUM(C.Total) AS 'Soma do Valor Total'
+FROM Conta C
+WHERE HoraFechamento IS NULL;
+
+-- Liste o nome dos garçons que nunca fizeram nenhum pedido.
+SELECT U.Login
+FROM Usuario U
+LEFT JOIN Pedido P ON U.CodUsuario = P.Usuario
+WHERE P.CodPedido IS NULL;
+
+-- Mostre a média de preço de cada produto agrupado pelo tipo do produto.
+SELECT Tipo, AVG(Preco) AS 'Média de Preço'
 FROM Produto
-WHERE Tipo LIKE 'Software' OR Tipo LIKE 'Hardware';
+GROUP BY Tipo;
 
--- f) Liste o nome e cidade de todos os clientes que tem como sobrenome (final do nome) Silva.
-SELECT Nome, Cidade
-FROM Cliente
-WHERE Nome LIKE '%Silva';
-
--- g) Liste a média de preços dos pedidos (baseado na coluna ValorTotal)
-SELECT AVG(ValorTotal) AS 'Média de Preços'
-FROM Pedido
-
--- h) Liste a média de preço de cada tipo de produto. Ordene por tipo.
-SELECT Tipo, AVG(Preco) AS 'Média de Preços'
-FROM Produto
-GROUP BY Tipo
-ORDER BY Tipo;
-
--- i) Liste o nome do produto com o preço mais alto.
-SELECT TOP 1 NomeProduto
-FROM Produto
-ORDER BY Preco DESC;
-
--- j) Liste o preço do produto com o preço mais baixo.
-SELECT TOP 1 Preco
-From Produto
-ORDER BY Preco ASC;
-
--- k) Altere a tabela Pedido para incluir uma coluna chamada Data do tipo timestamp null.
-ALTER TABLE Pedido
-ADD Data datetime NULL;
-
--- l) Atualize a coluna Data da tabela Pedido para 01/01/2022 se o cliente for de Curitiba.
--- E para 01/05/2022 se o cliente for de outra cidade.
-UPDATE Pedido
-SET Data = CASE 
-    WHEN c.Cidade = 'Curitiba' THEN '2022-01-01'
-    ELSE '2022-05-01'
-END
-FROM Pedido p
-INNER JOIN Cliente c ON p.Cliente = c.CodCliente;
-
--- m) Listar a data, valor unitário e valor total de todos os pedidos e também listar se
--- o valor total está de acordo com o valor unitário multiplicado pela quantidade.
-SELECT 
-    Data,
-    ValorUnitario,
-    ValorTotal,
-    CASE 
-        WHEN ValorTotal = ValorUnitario THEN 'CORRETO'
-        ELSE 'INCORRETO'
-    END AS 'Status do Valor Total'
-FROM Pedido;
-
--- n) Liste o nome do cliente e do produto de cada pedido do cliente 1. Não duplique as informações.
-SELECT DISTINCT C.Nome, Pr.NomeProduto
-FROM Pedido Pe
+-- Mostre a soma de todos os produtos consumidos para a conta cod = 10.
+SELECT Co.CodConta, Co.Mesa, Co.Situacao, Co.Total, SUM(Pr.Preco) AS 'Soma dos produtos consumidos pela conta 10'
+FROM Conta Co
+INNER JOIN Pedido Pe ON Co.CodConta = Pe.Conta
 INNER JOIN Produto Pr ON Pr.CodProduto = Pe.Produto
-INNER JOIN Cliente C ON C.CodCliente = Pe.Cliente
-WHERE C.CodCliente = 1;
-
--- o) Liste o nome de todos os produtos que não foram vendidos.
-SELECT Pr.NomeProduto
-FROM Produto Pr
-LEFT JOIN Pedido Pe ON Pe.Produto = Pr.CodProduto
-WHERE Pe.Produto IS NULL;
-
--- p) Una a listagem do código e nome de todos os produtos com o código e nome de todos os clientes.
-SELECT Pr.CodProduto, Pr.NomeProduto, C.CodCliente, C.Nome
-FROM Produto Pr
-LEFT JOIN Pedido Pe ON Pr.CodProduto = Pe.Produto
-LEFT JOIN Cliente C ON Pe.Cliente = C.CodCliente
-
--- q) Liste a quantidade de clientes, quantidade de pedidos e preço médio dos
--- produtos usando um único select com subselects.
-SELECT
-    (SELECT COUNT(*) FROM Cliente) AS 'Quantidade de Clientes',
-    (SELECT COUNT(*) FROM Pedido) AS 'Quantidade de Pedidos',
-    (SELECT AVG(Preco) FROM Produto) AS 'Preço Médio dos Produtos';
-
--- r) Liste o nome do cliente, soma do valor dos pedidos, média do valor dos pedidos e
--- quantidade de pedidos dos clientes qque já compraram mais de 15 mil (soma do valor total)
-SELECT
-    c.Nome AS 'Nome do Cliente',
-    SUM(pd.ValorTotal) AS 'Soma dos Pedidos',
-    AVG(pd.ValorTotal) AS 'Média dos Pedidos',
-    COUNT(pd.CodPedido) AS 'Quantidade de Pedidos'
-FROM Cliente c
-INNER JOIN Pedido pd ON c.CodCliente = pd.Cliente
-GROUP BY c.CodCliente, c.Nome
-HAVING SUM(pd.ValorTotal) > 15000;
+GROUP BY Co.CodConta, Co.Mesa, Co.Situacao, Co.Total;
